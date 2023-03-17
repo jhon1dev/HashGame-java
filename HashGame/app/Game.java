aabc2cd1f0977b433da78967e8e1f140f8555de1
Lea1dev <125105950+Lea1dev@users.noreply.github.com>
2023-03-16 23:33:58 -0300

    Upload dos arquivos

---
+++ HashGame/app/Game.java
@@ -0,0 +1,15 @@
+package app;
+
+public class Game extends Frame{
+	
+	private static final long serialVersionUID = 2L;
+
+	
+	public Game() {
+		
+	}
+	
+	public static void main(String[] args) {
+		var game = new Game();
+	}
+}
---
+++ HashGame/app/Panel.java
@@ -0,0 +1,15 @@
+package app;
+
+import java.awt.Color;
+
+import javax.swing.BorderFactory;
+import javax.swing.JPanel;
+
+public class Panel extends JPanel{
+	
+	public Panel() {
+		this.setBorder(BorderFactory.createMatteBorder(2,3,2,3,Color.WHITE));
+		this.setBackground(Color.ORANGE);
+	}
+
+}
